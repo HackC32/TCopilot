89d6431913ae62e8b480069e4424f0d7280b1564
HackC32 <94924667+HackC32@users.noreply.github.com>
2026-07-03 16:51:57 +0000

    COMMITCOB01

--- test07/progCB1.cbl
+++ test07/progCB1.cbl
@@ -1,4 +1,16 @@
-      *        PRIMEIRO TESTE COM COMMIT (GITHUB) - 07/06/2026 
-       display "OLA MUNDO!"    
-       DISPLAY "TESTE COMMIT GITHUB - 07/06/2026"
-       DISPLAY "FINAL TEST"
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID COB01CH001.
+       environment division.
+      *        PRIMEIRO TESTE COM COMMIT (GITHUB) - 07/06/2026
+       configuration section.
+       
+       data division.
+       working-storage section.
+       
+       procedure division.
+           DISPLAY "OLA MUNDO!"    
+           DISPLAY "TESTE COMMIT GITHUB - 07/06/2026"
+           DISPLAY "FINAL TEST"
+             goback.
+       
+       end program COB01CH001.
